3d60b544432e79f331805084d13754b18ecb9e96
Nderim Bytyqi <bytyqinderim87@gmail.com>
2021-07-29 09:55:19 +0200

    main: Project setup

---
+++ src/main/java/com/xogito/userprojectmanagement/UserProjectManagementApplication.java
@@ -0,0 +1,13 @@
+package com.xogito.userprojectmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserProjectManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserProjectManagementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xogito/userprojectmanagement/UserProjectManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xogito.userprojectmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserProjectManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
